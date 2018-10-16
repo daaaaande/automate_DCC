f445070de93b5fecbaea703fffa52aaf365a7580
daaaaande <worldtour@live.de>
2018-10-16 11:42:10 +0200

    quickfix matrixmaker V2 command

--- auto_automaker.pl
+++ auto_automaker.pl
@@ -73,7 +73,7 @@ foreach my $singleline (@lines){
 foreach my $groupname (@groups){
 	my$errseding=system("sed -i '1d' $groupname/*.tsv"); # will remove first line from steptwo output i.e headers
 	my$errcat=system("cat $groupname/*.tsv >$groupname/allsites_bedgroup_$groupname.csv");
-	my$errmatxrix=system("nice perl auto_find_circ/matrixmaker-V2.pl $groupname/allsites_bedgroup_$groupname.csv $groupname/allcircs_matrixout.txt");
+	my$errmatxrix=system("nice perl  automate_DCC/matrixmaker-V2.pl $groupname/allsites_bedgroup_$groupname.csv $groupname/allcircs_matrixout.txt");
 	my$matrtmaker=system("perl automate_DCC/matrixtwo.pl $groupname/allcircs_matrixout.txt $groupname/allc_matrixtwo.tsv");
 	print ER "errors catting $groupname .csv files together:\n$errcat\n";
 	system("cp $groupname/allsites_bedgroup_$groupname.csv $ndir/");
@@ -84,7 +84,7 @@ foreach my $groupname (@groups){
 }
 
 my$erralcat=system("cat $ndir/* >$ndir/$ndir.allbeds.dcc.out");
-my$erralm1=system("perl nice perl auto_find_circ/matrixmaker-V2.pl $ndir/$ndir.allbeds.dcc.out $ndir/allsamples_matrix.dcc.tsv");
+my$erralm1=system("nice perl automate_DCC/matrixmaker-V2.pl $ndir/$ndir.allbeds.dcc.out $ndir/allsamples_matrix.dcc.tsv");
 my$err_mat2=system("perl automate_DCC/matrixtwo.pl $ndir/allsamples_matrix.dcc.tsv $ndir/allsamples_m_heatmap.dcc.tsv");
 
 print ER "error making files in $ndir :\ncat:\t$erralcat\nmatrix 1 creation:\t$erralm1 \nmatrix 2 creation:\n$err_mat2\n";
