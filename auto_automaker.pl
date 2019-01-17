3636003dcc35077d170f8321c09b072dcc5fd766
daaaaande <worldtour@live.de>
2019-01-17 10:06:35 +0100

    switching from system() to ``

--- auto_automaker.pl
+++ auto_automaker.pl
@@ -1,7 +1,7 @@
 #/usr/bin/perl -w
 use strict;
 
-system("clear");
+`clear`;
 
 # cd /find_circ/dcc/automate_DCC
 # copy infile to /find_circ/dcc/
@@ -37,7 +37,7 @@ foreach my $singleline (@lines){
 		my$samplename=$lineparts[2];
 			$samplename=~s/\-/_/g;# avoid - in samplenames
 		my$groupname=$lineparts[3];
-		if($groupname eq ''){ # empty or only space-char
+		if($groupname eq ""){ # empty or only space-char
 			$groupname="default_group_$ndir";
 		}
 		chomp $samplename;
@@ -47,22 +47,22 @@ foreach my $singleline (@lines){
 		print ER "##############################################################\n";
 		print ER "starting @ $tim \nfinding circs in sample $samplename with DCC ...\n";
 
-		$error=system("perl automate_DCC/dcc_starter.pl $fileone $filetwo $samplename");
-		#my$err2=system("perl  run_$samplename/CIRCexplorer_circ.txt run_$samplename/$samplename.processed.tsv $samplename");
+		$error=`perl automate_DCC/dcc_starter.pl $fileone $filetwo $samplename`;
+		#my$err2=`perl  run_$samplename/CIRCexplorer_circ.txt run_$samplename/$samplename.processed.tsv $samplename`;
 		# will dump file into run_$samplename/$samplename_processed.tsv, this to be done for every file
 
 		print ER "errors:\n$error\n\n";
-		$errthre=system("rm run_$samplename/$samplename* ");
+		$errthre=`rm run_$samplename/$samplename* `;
 		print ER "errors removing tmpfiles for sample $samplename:\n $errthre\n";
 		#deleting the copies of .fastq files afterwards in dcc and circexplorer to not crowd the hdd
-	#	my$erdel=system("rm $fileone $filetwo");
+	#	my$erdel=`rm $fileone $filetwo`;
 		print ER "errors removing fastqs for sample $samplename:\n \n";
 		if($groupname=~/[a-z]/gi){
 			if(!(grep(/$groupname/,@groups))){ # check if group already present
 				mkdir $groupname;		# IF NOT, MAKE GROUPDIR
 				push(@groups,$groupname);
 			}
-		$errortwo=system ("cp run_$samplename/processed_run_$samplename.tsv $groupname/");
+		$errortwo=`cp run_$samplename/processed_run_$samplename.tsv $groupname/`;
 		}
 
 		print ER "errors auto_moving:\n$errortwo\n";
@@ -75,28 +75,28 @@ foreach my $singleline (@lines){
 
 
 foreach my $groupname (@groups){
-	my$errseding=system("sed -i '1d' $groupname/*.tsv"); # will remove first line from steptwo output i.e headers
-	my$errcat=system("cat $groupname/*.tsv >$groupname/allsites_bedgroup_$groupname.csv");
-	my$errmatxrix=system("nice perl  automate_DCC/matrixmaker-V3_A.pl $groupname/allsites_bedgroup_$groupname.csv $groupname/allcircs_matrixout.txt");
-	my$matrtmaker=system("perl automate_DCC/matrixtwo.pl $groupname/allcircs_matrixout.txt $groupname/allc_matrixtwo.tsv");
+	my$errseding=`sed -i '1d' $groupname/*.tsv`; # will remove first line from steptwo output i.e headers
+	my$errcat=`cat $groupname/*.tsv >$groupname/allsites_bedgroup_$groupname.csv`;
+	my$errmatxrix=`nice perl  automate_DCC/matrixmaker-V3_A.pl $groupname/allsites_bedgroup_$groupname.csv $groupname/allcircs_matrixout.txt`;
+	my$matrtmaker=`perl automate_DCC/matrixtwo.pl $groupname/allcircs_matrixout.txt $groupname/allc_matrixtwo.tsv`;
 	print ER "errors catting $groupname .csv files together:\n$errcat\n";
-	system("cp $groupname/allsites_bedgroup_$groupname.csv $ndir/");
+	`cp $groupname/allsites_bedgroup_$groupname.csv $ndir/`;
 
 	print ER "errors making matrix for $groupname/allsites_bedgroup_$groupname.csv :\n$errmatxrix\n";
 	print ER "errors making second matrix for $groupname/allsites_bedgroup_$groupname.csv :\n$matrtmaker\n";
 
 }
 
-my$erralcat=system("cat $ndir/*.csv >$ndir/$ndir.allbeds.dcc.out");
+my$erralcat=`cat $ndir/*.csv >$ndir/$ndir.allbeds.dcc.out`;
 my$er_check_in=`perl automate_DCC/matrix_infile_checker.pl $ndir/$ndir.allbeds.dcc.out`;
 print "checking the infile for DCC matrix in $ndir ...\n$er_check_in \n";
-my$erralm1=system("nice perl automate_DCC/matrixmaker-V3_A.pl $ndir/$ndir.allbeds.dcc.out $ndir/allsamples_matrix.dcc.mat1");
-my$err_mat2=system("perl automate_DCC/matrixtwo.pl $ndir/allsamples_matrix.dcc.mat1 $ndir/allsamples_m_heatmap.dcc.mat2");
+my$erralm1=`nice perl automate_DCC/matrixmaker-V3_A.pl $ndir/$ndir.allbeds.dcc.out $ndir/allsamples_matrix.dcc.mat1`;
+my$err_mat2=`perl automate_DCC/matrixtwo.pl $ndir/allsamples_matrix.dcc.mat1 $ndir/allsamples_m_heatmap.dcc.mat2`;
 
 print ER "error making files in $ndir :\ncat:\t$erralcat\nmatrix 1 creation:\t$erralm1 \nmatrix 2 creation:\n$err_mat2\n";
 
 # now copy two matrix files into find_circ dir
-my$errtransfer=system("cp $ndir/*.mat* /media/daniel/NGS1/RNASeq/find_circ/$ndir/");
+my$errtransfer=`cp $ndir/*.mat* /media/daniel/NGS1/RNASeq/find_circ/$ndir/`;
 print ER "transfering matrix to find_circ dir errors: \n$errtransfer\n";
 
 print ER "finished with all groups\n";
--- dcc_starter.pl
+++ dcc_starter.pl
@@ -8,8 +8,8 @@ my$starttime= time;
 
 open(ER,'>>',"/home/daniel/logfile_auto.log")||die "$!";		# global logfile
 
-system("rm Chimeric.out.junction");
-system("rm fusion_junction.txt");
+`rm Chimeric.out.junction`;
+`rm fusion_junction.txt`;
 
 # first get reference place for hg19
 my$bowtwiindexplace="/media/daniel/NGS1/RNASeq/find_circ/dcc/";
@@ -26,7 +26,7 @@ chomp $infile2;
 my$samplename=$ARGV[2];
 chomp $samplename;
 
-print ER "-------------------------------------------------\nsample $samplename processing:\n";
+print ER "-\nsample $samplename DCC  processing:\n";
 #### start of dcc for this sample
 mkdir "run_$samplename";
 
@@ -38,7 +38,7 @@ chdir "run_$samplename";
 print ER "doing mate STAR alignment...\n";
 # start DCC in find_circ/dcc/
 # STAR --runThreadN 10 --genomeDir . --outSAMtype BAM SortedByCoordinate --readFilesIn HAL01_R1_trimmed.fastq HAL01_R2_trimmed.fastq --outFileNamePrefix halo1_r --outReadsUnmapped Fastx --outSJfilterOverhangMin 15 15 15 15 --alignSJoverhangMin 15 --alignSJDBoverhangMin 15 --outFilterMultimapNmax 20 --outFilterScoreMin 1 --outFilterMatchNmin 1 --outFilterMismatchNmax 2 --chimSegmentMin 15 --chimScoreMin 15 --chimScoreSeparation 10 --chimJunctionOverhangMin 15
-my$tophatout=system("STAR --runThreadN 10 --genomeDir ../ --outSAMtype BAM SortedByCoordinate --readFilesIn ../../$infile1 ../../$infile2 --outFileNamePrefix $samplename. --outReadsUnmapped Fastx --outSJfilterOverhangMin 15 15 15 15 --alignSJoverhangMin 15 --alignSJDBoverhangMin 15 --outFilterMultimapNmax 20 --outFilterScoreMin 1 --outFilterMatchNmin 1 --outFilterMismatchNmax 2 --chimSegmentMin 15 --chimScoreMin 15 --chimScoreSeparation 10 --chimJunctionOverhangMin 15");
+my$tophatout=`STAR --runThreadN 10 --genomeDir ../ --outSAMtype BAM SortedByCoordinate --readFilesIn ../../$infile1 ../../$infile2 --outFileNamePrefix $samplename. --outReadsUnmapped Fastx --outSJfilterOverhangMin 15 15 15 15 --alignSJoverhangMin 15 --alignSJDBoverhangMin 15 --outFilterMultimapNmax 20 --outFilterScoreMin 1 --outFilterMatchNmin 1 --outFilterMismatchNmax 2 --chimSegmentMin 15 --chimScoreMin 15 --chimScoreSeparation 10 --chimJunctionOverhangMin 15`;
 # creates auto_$samplename dir in test/
 print ER "errors during STAR alignment:\n $tophatout\n";
 
@@ -52,12 +52,12 @@ my$lanetwoname="lane_2$samplename";
 # alignment first read .fastq file
 print ER "doing line 1 STAR alignment...\n";
 
-my$err_star_line1=system("STAR --runThreadN 10 --genomeDir ../ --outSAMtype None --readFilesIn ../../$infile1 --outFileNamePrefix $laneonename. --outReadsUnmapped Fastx --outSJfilterOverhangMin 15 15 15 15 --alignSJoverhangMin 15 --alignSJDBoverhangMin 15 --seedSearchStartLmax 30 --outFilterMultimapNmax 20 --outFilterScoreMin 1 --outFilterMatchNmin 1 --outFilterMismatchNmax 2 --chimSegmentMin 15 --chimScoreMin 15 --chimScoreSeparation 10 --chimJunctionOverhangMin 15");
+my$err_star_line1=`STAR --runThreadN 10 --genomeDir ../ --outSAMtype None --readFilesIn ../../$infile1 --outFileNamePrefix $laneonename. --outReadsUnmapped Fastx --outSJfilterOverhangMin 15 15 15 15 --alignSJoverhangMin 15 --alignSJDBoverhangMin 15 --seedSearchStartLmax 30 --outFilterMultimapNmax 20 --outFilterScoreMin 1 --outFilterMatchNmin 1 --outFilterMismatchNmax 2 --chimSegmentMin 15 --chimScoreMin 15 --chimScoreSeparation 10 --chimJunctionOverhangMin 15`;
 print ER "errors during STAR lane 1 alignment:\n $err_star_line1\n";
 
 print ER "doing line 2 STAR alignment...\n";
 
-my$err_star_line2=system("STAR --runThreadN 10 --genomeDir ../ --outSAMtype None --readFilesIn ../../$infile2 --outFileNamePrefix $lanetwoname. --outReadsUnmapped Fastx --outSJfilterOverhangMin 15 15 15 15 --alignSJoverhangMin 15 --alignSJDBoverhangMin 15 --seedSearchStartLmax 30 --outFilterMultimapNmax 20 --outFilterScoreMin 1 --outFilterMatchNmin 1 --outFilterMismatchNmax 2 --chimSegmentMin 15 --chimScoreMin 15 --chimScoreSeparation 10 --chimJunctionOverhangMin 15");
+my$err_star_line2=`STAR --runThreadN 10 --genomeDir ../ --outSAMtype None --readFilesIn ../../$infile2 --outFileNamePrefix $lanetwoname. --outReadsUnmapped Fastx --outSJfilterOverhangMin 15 15 15 15 --alignSJoverhangMin 15 --alignSJDBoverhangMin 15 --seedSearchStartLmax 30 --outFilterMultimapNmax 20 --outFilterScoreMin 1 --outFilterMatchNmin 1 --outFilterMismatchNmax 2 --chimSegmentMin 15 --chimScoreMin 15 --chimScoreSeparation 10 --chimJunctionOverhangMin 15`;
 print ER "errors during STAR lane 2 alignment:\n $err_star_line2\n";
 
 
@@ -71,7 +71,7 @@ print ER "errors during STAR lane 2 alignment:\n $err_star_line2\n";
 my$bothlanesname="$samplename.Chimeric.out.junction";
 ##
 print ER "running dcc..\n";
-my$dcc_err=system("DCC $bothlanesname -mt1 $laneonename.Chimeric.out.junction -mt2 $lanetwoname.Chimeric.out.junction -D -fg -an ../hg19_ens.gtf -Pi -M -Nr 2 1 -A ../hg19.fa -N -T 10 -an ../all_ref.gtf");
+my$dcc_err=`DCC $bothlanesname -mt1 $laneonename.Chimeric.out.junction -mt2 $lanetwoname.Chimeric.out.junction -D -fg -an ../hg19_ens.gtf -Pi -M -Nr 2 1 -A ../hg19.fa -N -T 10 -an ../all_ref.gtf`;
 print ER "errors running dcc: $dcc_err\n";
 
 # seding, removing headers
@@ -82,14 +82,14 @@ print "errors removing headers:\n$sed\n";
 
 # bedtools annotations
 print ER "using bedtools for annotation of CircRNACount file for $samplename...\n";
-my$betout=system("bedtools window -a CircRNACount -b /media/daniel/NGS1/RNASeq/find_circ/bed_files/Genes_RefSeq_hg19_09.20.2013.bed -w 1 >CircRNACount_annotated.tsv");
+my$betout=`bedtools window -a CircRNACount -b /media/daniel/NGS1/RNASeq/find_circ/bed_files/Genes_RefSeq_hg19_09.20.2013.bed -w 1 >CircRNACount_annotated.tsv`;
 print ER "errors annotating $samplename :\n$betout\n";
 
 
 
 # now parsing the output
 print ER "parsing in run_$samplename ...\n";
-my$err_running_dcc_outreader=system("perl ../automate_DCC/dcc_outreader.pl CircRNACount_annotated.tsv CircCoordinates processed_run_$samplename.tsv $samplename");
+my$err_running_dcc_outreader=`perl ../automate_DCC/dcc_outreader.pl CircRNACount_annotated.tsv CircCoordinates processed_run_$samplename.tsv $samplename`;
 print ER "errors parsing in run_$samplename/ : \n$err_running_dcc_outreader\n";
 
 print ER "############################################################\nsample $samplename done :\n";
